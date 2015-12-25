b926631ea6c85c6666214354f4a9e918bafd7d01
Ajaythota <ajaykumarthota@yahoo.com>
2015-12-24 19:03:18 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+##This is Week 3 programming Assignment2 
+## This function calculates the inverse of matrix using R function called Solve
+## The purpose of this function is to access the final value of common operations like mean, sum ..etc by just calling the function
+## and accessing the ready answer instead of doing the operations everytime. 
+## <<- acts like a global  or static in Java analogy
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+M <- matrix(c(1,2,3,4),2,2)
 
+makeCacheMatrix <- function(x= numeric()) {
+  i <- NULL
+  
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+    
+  }
+  
+  get <- function() x
+  
+  setInv <- function(inv= numeric()) i <<- inv
+  getInv <- function() i
+  
+  
+  list(set=set,get=get,setInv=setInv,getInv=getInv)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x,...){
+  i <- x$getInv()
+  if (!is.null(i)){
+    message ("getting from cache")
+    return(i)
+    
+  }
+  
+  message ("not  getting from cache")
+  matrix <- x$get()
+  i <- solve(matrix)
+  x$setInv(i)
+  i
+  
 }
